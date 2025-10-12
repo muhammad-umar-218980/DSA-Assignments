4482425b96ba3cb6587fc10fc6d5d4f4f5a056bb
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-13 02:22:36 +0500

    Added Q6

--- DSA Assignment 2/Q6_ CT-28.cpp
+++ DSA Assignment 2/Q6_ CT-28.cpp
@@ -7,91 +7,96 @@ using namespace std;
 	Class : CSIT Section A Batch 24
 */
 
-class Node{
-public: 
-	int data;
-	int minVal;
-	Node* next;
-	
-	Node(int data){
-		this->data = data;
-		next = nullptr;
-	}
-};
-
-class Stack{
-private:
-	Node* top;
-	
+class Node {
 public:
-	Stack() {
-		top = nullptr;
-	}
-	
-	void push(int val){
-		Node* n = new Node(val);
-		if(empty()) {
-			n->minVal = val;
-			top = n;
-			return;
-		}
-		
-		if(val < top->minVal) n->minVal = val;
-		else n->minVal = top->minVal;
-		
-		n->next = top;
-		top = n;		
-	}
-	
-	int pop(){
-		if(empty()) return -1;
-		
-		int value = top->data;
-		Node* temp = top;
-		top = top->next;
-		delete temp;
-		return value;
-	}
-	
-	bool empty(){
-		return top == nullptr;
-	}
-	
-	int getMin() {
-		if(empty()) return -1;
-		return top->minVal;
-	}
-	
-	void print(){
-		if(empty()){
-			cout << "\nStack is empty ";
-			return;
-		}
-		
-		Node* current = top;
-		cout << "\nStack : \n";
-		while(current != nullptr){
-			cout << current->data << endl;
-			current = current->next;
-		}
-	}
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        next = nullptr;
+    }
 };
 
-int main(){
-	Stack st;
-	for(int i = 1 ; i <= 10 ;i++) st.push(i);
-	
-	st.print();
-	
-	cout << "\nMinimum value in stack is : " << st.getMin() <<endl;
-	
-	st.push(-100);
-	st.print();
-	cout << "\nMinimum value in stack is : " << st.getMin();
-	
-	st.pop();
-	st.print();
-	cout << "\nMinimum value in stack is : " << st.getMin();
-	
-	return 0;
+class Stack {
+private:
+    Node* top;
+    int minVal;
+
+public:
+    Stack() {
+        top = nullptr;
+        minVal = INT_MAX;
+    }
+
+    void push(int val) {
+        Node* n = new Node(val);
+
+        if (empty()) minVal = val;
+        else minVal = min(val, minVal);
+
+        n->next = top;
+        top = n;
+    }
+
+    int pop() {
+        if (empty()) return -1;
+
+        int value = top->data;
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+
+        if (!empty()) {
+            Node* current = top;
+            minVal = current->data;
+            while (current != nullptr) {
+                minVal = min(minVal, current->data);
+                current = current->next;
+            }
+        } else minVal = INT_MAX;
+
+        return value;
+    }
+
+    bool empty() {
+        return top == nullptr;
+    }
+
+    int get_min() {
+        if (empty()) return -1;
+        return minVal;
+    }
+
+    void print() {
+        if (empty()) {
+            cout << "\nStack is empty ";
+            return;
+        }
+
+        Node* current = top;
+        cout << "\nStack : \n";
+        while (current != nullptr) {
+            cout << current->data << endl;
+            current = current->next;
+        }
+    }
+};
+
+int main() {
+    Stack st;
+    for (int i = 1; i <= 10; i++) st.push(i);
+
+    st.print();
+    cout << "\nMinimum value in stack is : " << st.get_min() << endl;
+
+    st.push(-100);
+    st.print();
+    cout << "\nMinimum value in stack is : " << st.get_min() << endl;
+
+    st.pop();
+    st.print();
+    cout << "\nMinimum value in stack is : " << st.get_min() << endl;
+
+    return 0;
 }
