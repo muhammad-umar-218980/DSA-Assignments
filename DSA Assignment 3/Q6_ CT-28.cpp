2626ed55af001c716a833373faf9315745406d15
muhammad-umar-218980 <muhammad.umar.siddique.1502@gmail.com>
2025-10-13 22:53:08 +0500

    Added Q6

---
+++ DSA Assignment 3/Q6_ CT-28.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Name : Muhammad Umar
+	Roll no. : CT - 24028
+	Class : CSIT Section A Batch 24
+*/
+
+void towerOfHanoi(int n, char source, char destination, char auxiliary) {
+	if (n == 1) {
+		cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+		return;
+	}
+
+	towerOfHanoi(n - 1, source, auxiliary, destination);
+	cout << "Move disk " << n << " from " << source << " to " << destination << endl;
+	towerOfHanoi(n - 1, auxiliary, destination, source);
+}
+
+int main() {
+	int n;
+	cout << "Enter the number of disks: ";
+	cin >> n;
+
+	cout << "\nSteps to solve Tower of Hanoi:\n";
+	towerOfHanoi(n, 'A', 'C', 'B');
+
+	return 0;
+}
